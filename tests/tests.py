5b2c8888b5baf39948381cf16f83de2c5f3ed659
ncopiy <ncopiy@yandex.com>
2023-06-03 14:51:37 +0300

    Update tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,48 +1,55 @@
 import os.path
 from sys import exc_info
 
-from tests.utils import get_current_tb_extender, TRACEBLAME_GIT_PATH
+from tests.utils import get_current_tb_extender, TRACEBLAME_GIT_PATH, ALL_FILES_IN_TRACEBLAME_REPO, \
+    DIVIDE_TO_ZERO_LINE_NUMBER, divide_by_zero, DIVIDE_TO_ZERO_COMMIT_SHA, DIVIDE_TO_ZERO_CALLER_COMMIT_SHA, \
+    DIVIDE_TO_ZERO_CALLER_LINE_NUMBER
 from traceblame import iter_stacks
 
 
 def test_files_in_repo():
-    expected = {
-        '.github/workflows/pipeline.yml',
-        '.github/workflows/release.yml',
-        '.gitignore',
-        'traceblame/main.py',
-        'traceblame/__init__.py',
-        'requirements.txt',
-        'tests/__init__.py',
-        'tests/tests.py',
-        'tests/utils.py',
-        'setup/__init__.py',
-        'setup/setup.py',
-        'LICENSE',
-        'README.md',
-    }
     tb_extender = get_current_tb_extender()
-    assert sorted(tb_extender.files_in_repo) == sorted(expected)
-
-
-def divide():
-    return 10 / 0
+    assert sorted(tb_extender.files_in_repo) == sorted(ALL_FILES_IN_TRACEBLAME_REPO)
 
 
 def test_get_blame_repr():
     tb_extender = get_current_tb_extender()
     divide_to_zero_line_repr = tb_extender.get_blame_repr(
-        path=os.path.join(TRACEBLAME_GIT_PATH, "tests", "tests.py"),
-        line=29,
+        path=os.path.join(TRACEBLAME_GIT_PATH, "tests", "utils.py"),
+        line=DIVIDE_TO_ZERO_LINE_NUMBER,
     )
     assert divide_to_zero_line_repr is not None
-    assert divide_to_zero_line_repr.email == "ncopiy@ya.ru", str(divide_to_zero_line_repr)
-    assert divide_to_zero_line_repr.commit == "99b7c490fc0344b0f31a931f6c6f4c3b89c2da9e"
+    assert divide_to_zero_line_repr.email == "ncopiy@yandex.com", str(divide_to_zero_line_repr)
+    assert divide_to_zero_line_repr.commit == DIVIDE_TO_ZERO_COMMIT_SHA
+
+    invalid_line_repr = tb_extender.get_blame_repr(
+        path=os.path.join(TRACEBLAME_GIT_PATH, "tests", "ululu", "ulululululululu.py"),
+        line=777,
+    )
+    assert invalid_line_repr is None
+
+
+def test_get_last_commit():
+    tb_extender = get_current_tb_extender()
+
+    commit_of_valid_path = tb_extender.get_last_commit(
+        path=os.path.join(TRACEBLAME_GIT_PATH, "tests", "utils.py"),
+        line=DIVIDE_TO_ZERO_LINE_NUMBER,
+    )
+    assert commit_of_valid_path is not None
+    assert commit_of_valid_path.author.email == "ncopiy@yandex.com", str(commit_of_valid_path.author)
+    assert commit_of_valid_path.hexsha == DIVIDE_TO_ZERO_COMMIT_SHA
+
+    commit_of_invalid_path = tb_extender.get_last_commit(
+        path=os.path.join(TRACEBLAME_GIT_PATH, "tests", "ululu", "ulululululululu.py"),
+        line=777,
+    )
+    assert commit_of_invalid_path is None
 
 
 def test_sys_exc_info_without_blame():
     try:
-        divide()
+        divide_by_zero()
     except:  # noqa
         exc_type, exc_value, exc_traceback = exc_info()
         for tb in iter_stacks(exc_traceback):
@@ -53,21 +60,32 @@ def test_sys_exc_info_with_blame():
     tb_extender = get_current_tb_extender()
 
     try:
-        divide()
+        divide_by_zero()
     except:  # noqa
         exc_type, exc_value, exc_traceback = tb_extender.get_extended_exc_info()
-        assert exc_type == ZeroDivisionError
-        assert exc_value is not None
-        for tb in iter_stacks(exc_traceback):
-            assert "blame" in tb.tb_frame.f_locals
+    else:
+        assert False, "we expected exception"
 
-            # ref https://github.com/ncopiy/traceblame/commit/99b7c490fc0344b0f31a931f6c6f4c3b89c2da9e
-            assert "traceblame/tests/tests.py" in tb.tb_frame.f_code.co_filename
-            line = tb.tb_lineno
-            assert line in [
-                56,  # line number of `divide()` code in this function in `try` block
-                29,  # line number of `return 10 / 0` code in this file
-            ], line
+    assert exc_type == ZeroDivisionError
+    assert exc_value is not None
 
-            assert tb.tb_frame.f_locals["blame"].email == "ncopiy@ya.ru", str(tb.tb_frame.f_locals["blame"])
-            assert tb.tb_frame.f_locals["blame"].commit == "99b7c490fc0344b0f31a931f6c6f4c3b89c2da9e"
+    any_tb_at_utils = False
+
+    expected_sha_to_line = {
+        DIVIDE_TO_ZERO_COMMIT_SHA: DIVIDE_TO_ZERO_LINE_NUMBER,
+        DIVIDE_TO_ZERO_CALLER_COMMIT_SHA: DIVIDE_TO_ZERO_CALLER_LINE_NUMBER,
+    }
+
+    for tb in iter_stacks(exc_traceback):
+        assert "blame" in tb.tb_frame.f_locals
+
+        if "traceblame/tests/utils.py" in tb.tb_frame.f_code.co_filename:
+            any_tb_at_utils = True
+        else:
+            continue
+
+        assert tb.tb_frame.f_locals["blame"].email == "ncopiy@yandex.com", str(tb.tb_frame.f_locals["blame"])
+        assert tb.tb_frame.f_locals["blame"].commit in expected_sha_to_line, str(tb.tb_frame.f_locals["blame"])
+        assert tb.tb_lineno == expected_sha_to_line[tb.tb_frame.f_locals["blame"].commit], str(tb.tb_frame.f_locals["blame"])
+
+    assert any_tb_at_utils
--- tests/utils.py
+++ tests/utils.py
@@ -14,7 +14,10 @@ def divide_by_zero_caller():
 
 
 DIVIDE_TO_ZERO_LINE_NUMBER = 9
+DIVIDE_TO_ZERO_COMMIT_SHA = "fd1aba79fcf9a16b7be72b35e6b53f72d46aaacc"
+
 DIVIDE_TO_ZERO_CALLER_LINE_NUMBER = 13
+DIVIDE_TO_ZERO_CALLER_COMMIT_SHA = "38577f25ebb8b2bdc3bf8ec8e3898b6870b9b308"
 
 TRACEBLAME_GIT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
